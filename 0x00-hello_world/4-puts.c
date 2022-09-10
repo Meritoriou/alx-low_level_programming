65870e0d75d45800a750d5e4756a232c7d21a8cf
Meritoriou <Meritorious002@gmail.com>
2022-09-10 02:09:39 -0700

    my c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 /**
  *main- Serves as the entry point for the program
  *
- * Retun: always zero 
+ *Retun:always zero
  */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
