3d7dbe9ca12be4d6a371ccc622c5cc4bbd12da1f
Meritoriou <Meritorious002@gmail.com>
2022-09-11 18:03:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 
 /**
- *main- Serves as the entry point for the program
- *
- * Description: C program that print with put function
+ *main -Entry point 
  *
  *Retun:Always 0 (Success)
  */
@@ -13,5 +11,5 @@ int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			
-			return (0);
+		return (0);
 }
