020e27324581d21494b50dac8299b789fc3bff5b
Meritoriou <Meritorious002@gmail.com>
2022-09-15 09:00:30 -0700

    _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,26 @@
-#include <unistd.h>
+int main(void)                                                                                                                          
 
-/**
- *  _putchar – writes the character c to stdout
- *  @c: The charater to print
- *
- *  Return: On success 1.
- *  On erro, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+{                                                                                                                                       
+
+        _putchar('_');                                                                                                                  
+
+        _putchar('p');                                                                                                                  
+
+        _putchar('u');                                                                                                                  
+
+        _putchar('t');                                                                                                                  
+
+        _putchar('c');                                                                                                                  
+
+        _putchar('h');                                                                                                                  
+
+        _putchar('a');                                                                                                                  
+
+        _putchar('r');                                                                                                                  
+
+        _putchar('\n');                                                                                                                 
+
+                                                                                                                                        
+
+        return (0);                                                                                                                     
+
