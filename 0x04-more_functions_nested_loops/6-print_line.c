c66bc962b633e33706574ae83bfac55ab0ba6ab9
Meritoriou <Meritorious002@gmail.com>
2022-09-15 20:27:31 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,22 @@
-#include "main.h"
-
-/**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
- */
-
-void print_line(int n)
-{
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i;
-
-		for (i = 0; i < n; i++)
-		{
-			putchar('');
-		}
-
-		_putchar('\n');
-	}
+/**                                                                                                                                     
+ * print_line - a function that draws a straight line in the terminal.                                                                  
+ * followed by a new line.                                                                                                              
+ * @n: An input integer                                                                                                                 
+ * Return: Always 0                                                                                                                     
+ */                                                                                                                                     
+                                                                                                                                        
+void print_line(int n)                                                                                                                  
+                                                                                                                                        
+{                                                                                                                                       
+                                                                                                                                        
+        int i = 0;                                                                                                                      
+                                                                                                                                        
+        if (n > 0)                                                                                                                      
+        {                                                                                                                               
+                for (; i < n; i++)                                                                                                      
+                        putchar('');                                                                                                  
+        }                                                                                                                               
+                                                                                                                                        
+        _putchar('\n');                                                                                                                 
+                                                                                                                                        
 }
